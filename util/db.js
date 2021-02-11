1a2a299bd7d2dac3f64ec935cc26d792c5b1f500
ryand67 <rwdevenney@gmail.com>
2021-02-11 16:22:04 -0500

    db connect

---
+++ util/db.js
@@ -0,0 +1,12 @@
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+export default async function connect() {
+    if(!client.isConnected()) await client.connect();
+    const db = client.db('dates');
+    return { db, client };
+}
