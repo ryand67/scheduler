3b4e131f6db26b1c55d77c8702da56b93ecc549e
ryand67 <rwdevenney@gmail.com>
2021-02-11 16:05:18 -0500

    calendar set

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useState } from "react";
 import styles from '../styles/Home.module.css'
+import Datepicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
+import { addDays, setHours, setMinutes, getDay, format } from "date-fns";
 
 export default function Home() {
+  const [startDate, setStartDate] = useState(new Date());
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Scheduling App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
+      <Datepicker
+        minDate={new Date()}
+        inline
+        selected={startDate}
+        onChange={date => setStartDate(date)}
+      />
     </div>
   )
 }
