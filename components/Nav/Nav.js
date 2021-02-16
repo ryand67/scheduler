7740860e3a4540be13caaa7878d76c4bf1bd9870
ryand67 <rwdevenney@gmail.com>
2021-02-16 17:06:27 -0500

    alternating nav

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Nav.module.css';
 
 export default function Nav() {
+    const [location, setLocation] = useState();
+    useEffect(() => {
+        setLocation(window.location.pathname);
+    }, [])
     return (
         <div>
-            <a href='/'><img className={styles.nav} src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/calendar-512.png" alt=""/></a>
+            <a href={location === '/' ? '/booked' : '/'}><img className={styles.nav} src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/calendar-512.png" alt=""/></a>
         </div>
     )
 }
